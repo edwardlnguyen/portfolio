0189410a1f293b1daa5f2e6ca9e1c003eea2dd66
edward <edlnguye@ucsc.edu>
2015-06-29 23:01:23 -0700

    added balance

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629060155) do
+ActiveRecord::Schema.define(version: 20150630055237) do
 
   create_table "userposts", force: true do |t|
     t.text     "context"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150629060155) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "balance"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
